dd17bee22dca061e0ff8f39c2d7de670df09e3a0
Steve JobsEleanor <eleanorabrow@gmail.com>
2023-04-04 08:57:58 -0400

    added input and button

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -12,6 +12,20 @@ function App() {
 
       <h1>To-Do List App</h1>
 
+      {/* {INPUT AND BUTTON} */}
+
+
+      <input 
+      type='text'
+      placeholder="Add item here"
+      value={newItem}
+      onChange= {e => setNewItem(e.target)}
+      />
+
+      <button>Add Item</button>
+
+
+
     </div>
   );
 }
